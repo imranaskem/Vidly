4d3eca1e186e85cebecb6848e3b0874675129c1a
imranaskem <imran.askem@gmail.com>
2017-01-09 17:59:51 +0000

    Initial commit

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "John Williams", Id = 1 },
+                new Customer { Name = "Hans Zimmer", Id = 2 }
+            };
+
+            var viewModel = new CustomerList { Customers = customers };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "John Williams", Id = 1 },
+                new Customer { Name = "Hans Zimmer", Id = 2 }
+            };
+
+            Customer displayedCustomer = null;
+
+            foreach (var customer in customers)
+            {
+                if (customer.Id == id)
+                {
+                    displayedCustomer = customer;
+                }
+            }
+
+            if (displayedCustomer == null)
+            {
+                return HttpNotFound("Customer with that Id doesn't exist!");
+            }
+            else
+            {
+                return View(displayedCustomer);
+            } 
+
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+                        
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie { Name = "Wall-E", Id = 1 },
+                new Movie { Name = "Shrek", Id = 2 }
+            };
+
+            var viewModel = new MovieList
+            {
+                Movies = movies
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movies = new List<Movie>
+            {
+                new Movie { Name = "Wall-E", Id = 1 },
+                new Movie { Name = "Shrek", Id = 2 }
+            };
+
+            Movie displayedMovie = null;
+
+            foreach (var movie in movies)
+            {
+                if (movie.Id == id)
+                {
+                    displayedMovie = movie;
+                }
+            }
+
+            if (displayedMovie == null)
+            {
+                return HttpNotFound("No movie with that Id");
+            }
+            else
+            {
+                return View(displayedMovie);
+            }
+
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
